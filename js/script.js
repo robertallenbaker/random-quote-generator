0bf270e5e72cab4a5a6114c44f30b72842d3c01a
Robert Allen Baker <robertallenbaker@me.com>
2023-03-18 21:07:50 -0400

    Random Quote Button working -- needs if statement next

--- js/script.js
+++ js/script.js
@@ -10,20 +10,53 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source: 'Nelson Mandela',
+    citation: '1995'
+  },
+  {
+    quote: 'Time is precious, waste it wisely.',
+    source: 'K Bromberg',
+    citation: 'Slow Burn - A Driven Novel'
+  },
+  {
+    quote: 'Be yourself; everyone else is already taken.',
+    source: 'Oscar Wilde'
+  },
+  {
+    quote: 'A room without books is like a body without a soul.',
+    source: 'Marcus Tullius Cicero'
+  },
+  {
+    quote: 'You only live once, but if you do it right, once is enough',
+    source: 'Mae West'
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+let getRandomQuote = () => {
+  let rollQuote = Math.floor(Math.random() * (quotes.length));
+  return rollQuote;
+}
 
 /***
  * `printQuote` function
 ***/
+let printQuote = () => {
+  let randomQuote = getRandomQuote();
+  let quoteString = 
+    `
+      <p class="quote">${quotes[randomQuote].quote}</p>
+      <p class="source">${quotes[randomQuote].source}</p>
 
-
+    `;
+  document.getElementById('quote-box').innerHTML = quoteString; 
+}
 
 /***
  * click event listener for the print quote button
